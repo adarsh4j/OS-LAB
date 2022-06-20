42bd029a0916f31abb693fc301252ab85ddbecf9
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-06-20 09:31:44 +0530

    Update PrioritySchedule.c

--- PrioritySchedule.c
+++ PrioritySchedule.c
@@ -1,147 +1,73 @@
 #include<stdio.h>
-
- int main()
-
+struct process
 {
+    int WT,AT,BT,TAT,PT;
+};
 
-    int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp,at[20],s,tt,pt[20];
+struct process a[10];
 
-    float avg_wt,avg_tat;
-
-    printf("Enter number of process:");
-
+int main()
+{
+    int n,temp[10],t,count=0,short_p;
+    float total_WT=0,total_TAT=0,Avg_WT,Avg_TAT;
+    printf("Enter the number of the process\n");
     scanf("%d",&n);
-
-    printf("Enter priority (less number has high prority):\n");
-
-    for(i=0;i<n;i++)
-
+    printf("Enter the arrival time , burst time and priority of the process\n");
+    printf("AT BT PT\n");
+    for(int i=0;i<n;i++)
     {
-
-        printf("p%d:",i+1);
-
-        scanf("%d",&pt[i]);
-
-        p[i]=i+1;         
-
-    }
-
-    
-
-     for(i=0;i<n;i++)
-
-    {
-
-        s=i;
-
-        for(j=i+1;j<n;j++)
-
-        {
-
-            if(pt[j]<pt[s])
-
-                s=j;
-
-        }
-
-  
-
-        tt=at[i];
-
-        at[i]=at[s];
-
-        at[s]=tt;
-
-  
-
-        tt=p[i];
-
-        p[i]=p[s];
-
-        p[s]=tt;
-
+        scanf("%d%d%d",&a[i].AT,&a[i].BT,&a[i].PT);
         
-
-    }   
-
-    printf("Enter Arrival Time:\n");
-
-    for(i=0;i<n;i++)
-
-    {
-
-        printf("p%d:",i+1);
-
-        scanf("%d",&at[i]);
-
-        p[i]=i+1;         
-
-    } 
-
-    printf("Enter Burst Time:\n");
-
-    for(i=0;i<n;i++)
-
-    {
-
-        printf("p%d:",i+1);
-
-        scanf("%d",&bt[i]);
-
-        p[i]=i+1;         
-
-    }   
-
-    wt[0]=0;            
-
-  
-
-   
-
-    for(i=1;i<n;i++)
-
-    {
-
-        wt[i]=0;
-
-        for(j=0;j<i;j++)
-
-            wt[i]+=bt[j];
-
-  
-
-        total+=wt[i];
-
+        // copying the burst time in
+        // a temp array fot futher use
+        temp[i]=a[i].BT;
     }
-
-  
-
-    avg_wt=(float)total/n;      
-
-    total=0;
-
-  
-
-   printf("\nProcess\t\tArrival time\tBurst Time\t\tPriority\tWaiting Time\tTurnaround Time");
-
-    for(i=0;i<n;i++)
-
+    
+    // we initialize the burst time
+    // of a process with maximum 
+    a[9].PT=10000;
+    
+    for(t=0;count!=n;t++)
     {
-
-        tat[i]=wt[i] + bt[j]; 
-
-        total+=tat[i];
-
-       printf("\nP[%d]\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d",p[i],at[i],pt[i],bt[i],wt[i],tat[i]);
-
+        short_p=9;
+        for(int i=0;i<n;i++)
+        {
+            if(a[short_p].PT>a[i].PT && a[i].AT<=t && a[i].BT>0)
+            {
+                short_p=i;
+            }
+        }
+        
+        a[short_p].BT=a[short_p].BT-1;
+        
+        // if any process is completed
+        if(a[short_p].BT==0)
+        {
+            // one process is completed
+            // so count increases by 1
+            count++;
+            a[short_p].WT=t+1-a[short_p].AT-temp[short_p];
+            a[short_p].TAT=t+1-a[short_p].AT;
+            
+            // total calculation
+            total_WT=total_WT+a[short_p].WT;
+            total_TAT=total_TAT+a[short_p].TAT;
+            
+        }
     }
-
-  
-
-    avg_tat=(float)total/n;    
-
-    printf("\nAverage Waiting Time=%f",avg_wt);
-
-    printf("\nAverage Turnaround Time=%f\n",avg_tat);
-
+    
+    Avg_WT=total_WT/n;
+    Avg_TAT=total_TAT/n;
+    
+    // printing of the answer
+    printf("ID WT TAT\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d %d\t%d\n",i+1,a[i].WT,a[i].TAT);
+    }
+    
+    printf("Avg waiting time of the process  is %f\n",Avg_WT);
+    printf("Avg turn around time of the process is %f\n",Avg_TAT);
+    
+    return 0;
 }
